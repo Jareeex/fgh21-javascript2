3984cdc8fe06912d2ceb4b9deda6bd52c730bf40
Jareeex <baswarafajar@gmail.com>
2024-06-06 07:13:57 +0700

    Add files via upload

---
+++ task02.js
@@ -0,0 +1,42 @@
+const names = [
+    'Abigail', 'Alexandra', 'Alison',
+     'Amanda', 'Angela', 'Bella',
+     'Carol', 'Caroline', 'Carolyn', 
+     'Deirdre', 'Diana', 'Elizabeth', 
+     'Ella', 'Faith', 'Olivia', 'Penelope'
+    ];
+    function searchName(arr, cb){
+
+        let result = []
+        
+        for(let i=0; i < arr.length; i++){
+        const nama = arr[i]
+        result = [...result, nama]
+   
+    }
+    
+    
+        cb(result, 3)
+        
+ }
+        
+        function limit(arr, lim){
+        
+        let result = []
+        
+        for(let i=0; i < lim; i++){
+        
+        if(arr[i] !== undefined) {
+        
+        result = [...result, arr[i]]
+           
+        
+    }
+   
+ }
+        
+ console.log(result)
+        
+}
+        
+       searchName(names, limit)
