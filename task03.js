683ebd58ec69f65d3ea405b15a8250f27f474ac2
Jareeex <baswarafajar@gmail.com>
2024-06-07 06:59:01 +0700

    task03.js

--- task03.js
+++ task03.js
@@ -1,29 +1,40 @@
 function seleksiNilai(nilaiAwal, nilaiAkhir, dataNilai) {
-  if (nilaiAwal < nilaiAkhir) {
+    if (nilaiAwal >= nilaiAkhir) {
+      console.log("Nilai akhir harus lebih besar dari nilai awal");
+      return;
+  }
+
   if (dataNilai.length <= 5) {
-  return "jumlah angka dalam data array harus lebih dari 5";
-    }
-      let array = [];
-           for (let i = 0; i < dataNilai.length; i++) {
-                if (nilaiAwal <= dataNilai[i] && nilaiAkhir >= dataNilai[i]) {
-          
-          array.push(dataNilai[i]);
-          array.sort();
-        }
+      console.log("Jumlah angka dalam dataNilai harus lebih dari 5");
+      return;
+  }
+    let dataFilter = [];
+  for (let i = 0; i < dataNilai.length; i++) {
+      if (dataNilai[i] >= nilaiAwal && dataNilai[i] <= nilaiAkhir) {
+          dataFilter = [...dataFilter, dataNilai[i]]
       }
-           if (array.length == 0) {
-        return('nilai tidak ditemukan')
+  }
+  
+  for (let i = 0; i < dataFilter.length - 1; i++) {
+      for (let j = 0; j < dataFilter.length - 1 - i; j++) {
+          if (dataFilter[j] > dataFilter[j + 1]) {
+              let temp = dataFilter[j];
+              dataFilter[j] = dataFilter[j + 1];
+              dataFilter[j + 1] = temp;
+          }
       }
-      return array;
+  }
+
+  if (dataFilter.length > 0) {
+      console.log("Output:", dataFilter);
   } else {
-    return "nilai akhir harus lebih besar dari nilai awal";
+      console.log("Nilai tidak ditemukan");
   }
 }
-const hasil = seleksiNilai(5, 20, [2,25,4,14,17,30,8]);
-const hasil2 = seleksiNilai(15, 3, [2,25,4,14,17,30,8]);
-const hasil3 = seleksiNilai(4, 17, [2,25,4]);
-const hasil4 = seleksiNilai(5, 17, [2,25,4,1,30,18]);
-console.log(hasil);
-console.log(hasil2);
-console.log(hasil3);
-console.log(hasil4);
+
+const dataNilai = []
+
+seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8])
+seleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8])
+seleksiNilai(4, 17, [2, 25, 4])
+seleksiNilai(5, 17, [2, 25, 4, 1, 30, 18])
